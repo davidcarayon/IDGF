a91e93ad47a26b31e3c1c6474cbffa4cb19dfb44
David Carayon <carayon.david@gmail.com>
2019-02-20 15:13:57 +0100

    update before documenting

--- R/radar_IDGF.R
+++ R/radar_IDGF.R
@@ -10,7 +10,7 @@
 #' @importFrom magrittr %>%
 #' @export
 #'
-Radar_IDGF <- function(result_IDGF, lang = "FR"){
+Diagnostic_IDGF <- function(result_IDGF, lang = "FR"){
 
   if(lang == "FR") {
 
@@ -24,21 +24,25 @@ Radar_IDGF <- function(result_IDGF, lang = "FR"){
     dplyr::select(id_releve,MES:SAT) %>%
     dplyr::mutate_at(dplyr::vars(MES:SAT),as.numeric) %>%
     tidyr::gather(key = param, value = EQR, -id_releve) %>%
-    dplyr::mutate(radar_metric = 1 - EQR)
+    dplyr::mutate(radar_metric = 1 - EQR) %>%
+    dplyr::mutate(color_radar = ifelse(radar_metric < 0.4, yes = "low", no = "high"))
 
   for( i in id_releves) {
     plot<-result_graph %>%
       dplyr::filter(id_releve == i) %>%
     ggplot2::ggplot(ggplot2::aes(x = param, y = radar_metric)) +
-      ggplot2::geom_bar(stat = "identity", fill = "slateblue",color="black", alpha = 0.7) +
+      ggplot2::geom_bar(ggplot2::aes(fill = color_radar),stat = "identity",color="black", alpha = 0.7) +
+      ggplot2::scale_fill_manual(values = c("high" = "indianred2","low" = "steelblue2"))+
+      ggplot2::guides(fill = FALSE)+
       ggplot2::geom_text(ggplot2::aes(x = param, y = radar_metric, label = round(EQR,2)), nudge_y = 0.05) +
       ggplot2::ylim(0,1) +
       ggplot2::guides(fill = FALSE) +
       ggplot2::coord_polar() +
       ggplot2::theme_bw() +
+      ggplot2::theme(panel.grid.major = ggplot2::element_line(color = "grey75"))+
       ggplot2::labs(title = i) +
       ggplot2::theme(axis.text.y = ggplot2::element_blank(), axis.ticks.y = ggplot2::element_blank(), axis.title = ggplot2::element_blank()) +
-      ggplot2::theme(axis.text.x = ggplot2::element_text(face = "bold",size = 9))
+      ggplot2::theme(axis.text.x = ggplot2::element_text(face = "bold",size = 9,color="black"))
 
 
     print(plot)
@@ -61,21 +65,25 @@ Radar_IDGF <- function(result_IDGF, lang = "FR"){
       dplyr::select(id_sample,MES:SAT) %>%
       dplyr::mutate_at(dplyr::vars(MES:SAT),as.numeric) %>%
       tidyr::gather(key = param, value = EQR, -id_sample) %>%
-      dplyr::mutate(radar_metric = 1 - EQR)
+      dplyr::mutate(radar_metric = 1 - EQR) %>%
+      dplyr::mutate(color_radar = ifelse(1-EQR < 0.4, yes = "low", no = "high"))
 
     for( i in id_releves) {
       plot<-result_graph %>%
         dplyr::filter(id_sample == i) %>%
         ggplot2::ggplot(ggplot2::aes(x = param, y = radar_metric)) +
-        ggplot2::geom_bar(stat = "identity", fill = "slateblue",color="black", alpha = 0.7) +
+        ggplot2::geom_bar(ggplot2::aes(fill = color_radar),stat = "identity",color="black", alpha = 0.7) +
+        ggplot2::scale_fill_manual(values = c("high" = "indianred2","low" = "steelblue2"))+
+        ggplot2::guides(fill = FALSE)+
         ggplot2::geom_text(ggplot2::aes(x = param, y = radar_metric, label = round(EQR,2)), nudge_y = 0.05) +
         ggplot2::ylim(0,1) +
         ggplot2::guides(fill = FALSE) +
         ggplot2::coord_polar() +
         ggplot2::theme_bw() +
+        ggplot2::theme(panel.grid.major = ggplot2::element_line(color = "grey75"))+
         ggplot2::labs(title = i) +
         ggplot2::theme(axis.text.y = ggplot2::element_blank(), axis.ticks.y = ggplot2::element_blank(), axis.title = ggplot2::element_blank()) +
-        ggplot2::theme(axis.text.x = ggplot2::element_text(face = "bold",size = 9))
+        ggplot2::theme(axis.text.x = ggplot2::element_text(face = "bold",size = 9,color="black"))
 
 
       print(plot)
